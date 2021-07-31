2a5ea45bd0ae60285b0b44886d60abe873e61a3a
odunet <odunet2000@yahoo.com>
2021-08-01 02:04:20 +1000

    Implemented Emotion Stled Components

--- src/components/Layouts.js
+++ src/components/Layouts.js
@@ -1,6 +1,10 @@
+/** @jsx jsx */
 import React from 'react';
 import * as containerStyles from './Layouts.module.css';
+import styled from '@emotion/styled';
+import { css, jsx } from '@emotion/react';
 
+// Layout wrapper componenet
 const Layouts = ({ children }) => {
   return (
     <div className={`container ${containerStyles['genericDiv']}`}>
@@ -10,12 +14,30 @@ const Layouts = ({ children }) => {
   );
 };
 
+// Footer wrapper componenet
 const Footer = () => {
   return (
-    <footer className={`footer ${containerStyles['footer']}`}>
-      &copy; Copyright 2021
-    </footer>
+    <main
+      css={css`
+        font-style: italic;
+        &:hover {
+          cursor: pointer;
+        }
+      `}
+    >
+      <StyledNav color='green'>
+        <footer className={`footer ${containerStyles['footer']}`}>
+          &copy; Copyright 2021
+        </footer>
+      </StyledNav>
+    </main>
   );
 };
+
+// Emotion styled component
+const StyledNav = styled.div`
+  text-decoration: underline;
+  color: ${(props) => (props.color ? props.color : 'yellow')};
+`;
 
 export default Layouts;
