c300c2a3c64b43978a6ee732e68573a033f4a5db
odunet <odunet2000@yahoo.com>
2021-08-01 00:32:13 +1000

    Added global sass config, and style file

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,5 +40,6 @@ module.exports = {
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`, // Needed for dynamic images
+    `gatsby-plugin-sass`, //Sass plugin
   ],
 };
