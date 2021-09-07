e7f65f788c7c9cebb5a497c77256eaf782909233
Ayokunle Odutayo <40292574+odunet@users.noreply.github.com>
2021-09-07 19:39:59 +1000

    Updated package.json_fix_5

---
+++ scripts.js
@@ -0,0 +1,5 @@
+require('dotenv').config()
+
+console.log('+++++++++++++++++++++++++++++++');
+console.log(`The environmental variable dealerID_Cloud is: ${process.env.dealerID_Cloud}`);
+console.log('+++++++++++++++++++++++++++++++');
