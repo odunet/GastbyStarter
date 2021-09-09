b19d32e4261f32cb4c8437690553036ad4979eb2
Ayokunle Odutayo <40292574+odunet@users.noreply.github.com>
2021-09-09 15:38:10 +1000

    Updated script to check change

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 require('dotenv').config()
 
 console.log('+++++++++++++++++++++++++++++++');
-console.log(`The environmental variable dealerID_Cloud is: ${process.env.dealerID_Cloud}`);
+console.log(`The environment variable dealerID_Cloud is: ${process.env.dealerID_Cloud}`);
 console.log('+++++++++++++++++++++++++++++++');
