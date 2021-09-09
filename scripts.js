84e1f812e7cab7ac80b18a32e54b8f9444969722
Ayokunle Odutayo <40292574+odunet@users.noreply.github.com>
2021-09-09 15:54:58 +1000

    Added dotenv dependency

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-// require('dotenv').config()
+require('dotenv').config()
 
 console.log('+++++++++++++++++++++++++++++++');
 console.log(`The environment variable dealerID_Cloud is this: ${process.env.dealerID_Cloud}`);
